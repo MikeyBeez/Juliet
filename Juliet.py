3b27e478e586ef5ee26b6f6d2cf55af2beb55119
Mike Bee <mbonsign@gmail.com>
2020-04-18 13:53:26 -0400

    moved mycommand() to mycommand.py

--- Juliet.py
+++ Juliet.py
@@ -1,6 +1,5 @@
 #!/home/bard/miniconda3/envs/Juliet/bin/python
 ###############################################################################################
-######## IMPORT PYTHON3 MODULES
 import pyaudio
 import pyautogui
 import subprocess 
@@ -15,6 +14,7 @@ import psutil
 import sys
 from vosk import Model, KaldiRecognizer
 from SpeakAndHear import talktome
+from SpeakAndHear import mycommand
 ###############################################################################################
 ###############################################################################################
 #
@@ -67,25 +67,25 @@ def checkIfProcessRunning(processName):
 
 ###############################################################################################
 ######## STT SPEECH TO TEXT FUNCTION THAT RETURNS THE VARIABLE: command
-def myCommand():
-    # "listens for commands"
-    # We imported vosk up above.
-    p = pyaudio.PyAudio()
-    stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
-    stream.start_stream()
-    model = Model("model-en")
-    rec = KaldiRecognizer(model, 16000)
-    while True:
-        data = stream.read(2000)
-        if len(data) == 0:
-            break
-        if rec.AcceptWaveform(data):
-        #print(rec.Result())
-        # I commented out this line and added the 3 lines below
-            myResult = rec.Result()
-            myList = myResult.split("text")
-            command = myList[1]
-            return command
+#def myCommand():
+#    # "listens for commands"
+#    # We imported vosk up above.
+#    p = pyaudio.PyAudio()
+#    stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+#    stream.start_stream()
+#    model = Model("model-en")
+#    rec = KaldiRecognizer(model, 16000)
+#    while True:
+#        data = stream.read(2000)
+#        if len(data) == 0:
+#            break
+#        if rec.AcceptWaveform(data):
+#        #print(rec.Result())
+#        # I commented out this line and added the 3 lines below
+#            myResult = rec.Result()
+#            myList = myResult.split("text")
+#            command = myList[1]
+#            return command
 ######## END STT SPEECH TO TEXT FUNCTION THAT RETURNS THE VARIABLE: command
 ###############################################################################################
 
@@ -371,7 +371,7 @@ def main():
     print("Hello, Sir.  How can I be of assistance?")
 
     while True:
-        output = myCommand()[3:]
+        output = mycommand.myCommand()[3:]
         if 'juli' in output:
             print('The Julia responds:\n')
             assistant(output, playcounter)
