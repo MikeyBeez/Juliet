f8b41aff29ab4ececb5fbee698ba2820af523c57
Mike Bee <mbonsign@gmail.com>
2020-04-20 05:42:48 -0400

    point to parent

--- SpeakAndHear/mycommand.py
+++ SpeakAndHear/mycommand.py
@@ -6,7 +6,7 @@ from vosk import Model, KaldiRecognizer
 ################################################################################################
 ######Check Model 
 def CheckMyModel():
-    if not os.path.exists("model-en"):
+    if not os.path.exists("../model-en"):
         print ("Please download the model from https://github.com/alphacep/kaldi-android-demo/releases and unpack as 'model-en' in the current folder.")
         exit(1)
 ################################################################################################
