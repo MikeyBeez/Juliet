74602d2211abd985309662ae74bef582111ef7b2
Mike Bee <mbonsign@gmail.com>
2020-04-20 14:16:04 -0400

    Pimped out Juliet.py with mad comments!

--- Juliet.py
+++ Juliet.py
@@ -29,8 +29,6 @@ def myVars():
     playcounter = 1
     # totalsongstoplay is set below in main()
     global totalsongstoplay 
-    # Wakeword constant (not yet implemented.)
-    wakeWord = "juli" 
 ################################################################################################
 ###### End myVars.
 
@@ -39,32 +37,41 @@ def main():
     # Initialize.
     myVars()
     try:
+        #kaldi.Recognizer requires a model.  Make sure we have it.  Otherwise say where to get it.
         ij.CheckMyModel()
     except SystemExit as e:
         print(e)
+    # This is where to set the number of songs to play when you say "Julie play music."     
     totalsongstoplay = 2
     # End initialize.
 
+    # Say and print some helpful infomtion.
+    talktome.talkToMe("To get started, You can say Julie Julie help.")
+    print("To get started, You can say 'Julie Julie help.'")
+    talktome.talkToMe("How can I help?")
+    print("How can I help?")
+
     # Loop over and over to continuously execute multiple commands.
-
-    talktome.talkToMe("To get started, You can say julia help.")
-    print("To get started, You can say 'Julia help.'")
-    talktome.talkToMe("Hello, Sir.  How can I be of assistance?")
-    print("Hello, Sir.  How can I be of assistance?")
-
     while True:
-        # listen for command.
+        # listen for command. The listener logic is inside the myCommand function.
         output = mycommand.myCommand()[3:]
-        # Respond to wake words "Julie," "Julia," "Julius," or "Juliet."
+        # The assistant function responds to wake words "Julie," "Julia," "Julius," or "Juliet."
+        # It also gets whatever else you said, like "Julie what's up?""
         if 'juli' in output:
-            print('The Julia responds:\n')
+            print('Julia responds:\n')
+            # The assistant function performs whatever action is found that matches the variable named "output."
+            # Also, variables are passed in case you ask to play music.
             julibrain.assistant(output, playcounter, totalsongstoplay)
+            # Whatever you said is printed out, so you can see what Julie understood.
+            # This will help you speak more clearly, if you see she doesn't understand.
             print(output)
 
         elif '""' in output:
             pass
 ######## END MAIN FUNCTION
 
-######## CALL FUNCTION
+# None of the code up above this line runs unless main is called.
+
+######## CALL THE MAIN FUNCTION HERE
 main()
 ###############################################################################################
