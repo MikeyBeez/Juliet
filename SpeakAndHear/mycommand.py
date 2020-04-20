bff37c77054203b07c1c1f760d3365a572d06684
Mike Bee <mbonsign@gmail.com>
2020-04-20 05:37:09 -0400

    Fixed refactoring bugs

--- SpeakAndHear/mycommand.py
+++ SpeakAndHear/mycommand.py
@@ -5,7 +5,7 @@ from vosk import Model, KaldiRecognizer
 
 ################################################################################################
 ######Check Model 
-def CheckModel():
+def CheckMyModel():
     if not os.path.exists("model-en"):
         print ("Please download the model from https://github.com/alphacep/kaldi-android-demo/releases and unpack as 'model-en' in the current folder.")
         exit(1)
