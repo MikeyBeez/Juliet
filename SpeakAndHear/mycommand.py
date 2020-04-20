ee76fbd58a03da202db1beed24229a3c60e71c8c
Mike Bee <mbonsign@gmail.com>
2020-04-20 05:48:19 -0400

    removed CheckMyModel from mycommand.py

--- SpeakAndHear/mycommand.py
+++ SpeakAndHear/mycommand.py
@@ -3,17 +3,6 @@
 import pyaudio
 from vosk import Model, KaldiRecognizer
 
-################################################################################################
-######Check Model 
-def CheckMyModel():
-    if not os.path.exists("model-en"):
-        print ("Please download the model from https://github.com/alphacep/kaldi-android-demo/releases and unpack as 'model-en' in the current folder.")
-        exit(1)
-################################################################################################
-######End Check Model 
-
-
-
 def myCommand():
     # "listens for commands"
     # We imported vosk up above.
