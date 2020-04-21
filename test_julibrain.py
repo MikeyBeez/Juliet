83a16b6b7b0170d6526fe9095caf018be0385e73
Mike Bee <mbonsign@gmail.com>
2020-04-21 12:31:31 -0400

    playing with assert

--- test_julibrain.py
+++ test_julibrain.py
@@ -2,9 +2,11 @@ import unittest
 from GreyMatter import julibrain
 from SpeakAndHear import talktome
 
-test = 0
+test = True
 
 class TestBrain(unittest.TestCase):
     def test_open_reddit(self):
-         = julibrain.assistant('open reddit', 1, 2, test)
+        test_error = julibrain.assistant('open reddit', 1, 2, test)
+        print(self.assertEquals(test_error, 10))
+
         
