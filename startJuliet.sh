2d2bbf5aa0cd4a3d25ef82d3282e474e9994c6a1
Mike Bee <mbonsign@gmail.com>
2020-05-12 12:32:36 -0400

    I created a snapshot of Juliet and build a new env.

--- startJuliet.sh
+++ startJuliet.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/home/bard/miniconda3/envs/Juliet/bin/python /home/bard/Code/Juliet/Juliet.py 2>/dev/null
+/home/bard/miniconda3/envs/J2/bin/python /home/bard/Code/Juliet/Juliet.py 2>/dev/null
 
