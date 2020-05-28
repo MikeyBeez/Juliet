fdfeb44d580edbd0d284b93f00be72a52f969223
Mike Bee <mbonsign@gmail.com>
2020-05-28 14:47:52 -0400

    flake8

--- initualizejuliet.py
+++ initualizejuliet.py
@@ -1,10 +1,13 @@
 import os
 
 ##############################################################
-######Check Model. 
+# Check Model.
+
+
 def CheckMyModel():
     if not os.path.exists("model-en"):
-        print ("Please download the model from https://github.com/alphacep/kaldi-android-demo/releases and unpack as 'model-en' in the current folder.")
+        print("Please download the model from https://github.com/alphacep/kaldi-android-demo/releases")
+        print("and unpack as 'model-en' in the current folder.")
         exit(1)
 ##############################################################
-######End Check Model.
+# End Check Model.
