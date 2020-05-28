2325a719158c4d617b503fe0f79634e592d273ba
Mike Bee <mbonsign@gmail.com>
2020-05-28 14:37:57 -0400

    more Flake8

--- GreyMatter/julibrain.py
+++ GreyMatter/julibrain.py
@@ -7,36 +7,44 @@
 
 # Import all the required modules.
 # Pyaudio is for the microphone and may be required by mpg123.
-# Pyautogui is for moving the mouse around robotically and automating key presses.
+# Pyautogui is for moving the mouse around
+#  robotically and automating key presses.
 # Subprocess is for running operating system commands and programs.
-# Os is for access operating system calls.  For example, it is used to get the current working directory.
+# Os is for access operating system calls.  For example, it is
+# used to get the current working directory.
 # Webrowser is used to open and control whatever your default webbrowser is.
 # The time module give us access to time related functionality.
 # Re is python3's regular expression module.
 # Requests is used for making get requests to http servers.
 # Wikipedia is python3's module to access Wikipedia's API.
 # Random access's random generator functionality.
-# Psutils adds process utilities -- access information about processes running on the system.
-# Sys adds access to system commands. I don't seem to be using this module.  (Possibly remove.)
-# SpeakAndHear is a local module.  You'll find this is the SpeakAndHear subdirectory.
+# Psutils adds process utilities -- access information about
+# processes running on the system.
+# Sys adds access to system commands. I don't seem to be using this
+# module.  (Possibly remove.)
+# SpeakAndHear is a local module.  You'll find this is the
+# SpeakAndHear subdirectory.
 # SkeakAndHear has modules for speech to text and text to speech.
-# GreyMatter is the program's brain.  It contains a large if statement that contains
-# all the keywords and subsequent actions. I shouldn't need to load this, as I'm in this file already.
+# GreyMatter is the program's brain.  It contains a
+# large if statement that contains
+# all the keywords and subsequent actions. I shouldn't need to load
+# this, as I'm in this file already.
 # (Possibly delete "import GreyMatter.")
 
 ##############################################################
 
-import pyaudio
+# import pyaudio
 import pyautogui
 import subprocess
-import os
+# import os
 import webbrowser
-from time import localtime, strftime, sleep
+# from time import localtime, strftime, sleep
+from time import sleep
 import re
-import requests
+# import requests
 import wikipedia
 from random import randrange
-import psutil
+# import psutil
 # import sys  (Possibly delete this line.)
 from SpeakAndHear import talktome
 # from GreyMatter import julibrain  (Possibly delete this line.)
@@ -48,6 +56,7 @@ from SpeakAndHear import talktome
 # This is Juliet's brain.
 # All her commands and logic are called here.
 #############################################################
+
 
 def cleanj(command):
     command = command.replace("julia", "")
@@ -62,12 +71,12 @@ def cleanj(command):
 
 def assistant(command, playcounter, songs2play, runtest):
     '''
-    Check if command exists and execute corresponding action.  
+    Check if command exists and execute corresponding action.
     '''
 # Big If Statement for Executing Commands
 
 # Open Stuff
-    #print("test = " + str(test) +".")
+    # print("test = " + str(test) +".")
 
 # First command. This will open reddit in your browser.
 # -------------------------------------------------------------
@@ -159,10 +168,10 @@ def assistant(command, playcounter, songs2play, runtest):
 # -------------------------------------------------------------
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-# Next command will open a website in browser.  
+# Next command will open a website in browser.
 # This is the general way to open ANY website.
-# It uses the re (regular expressions) module.   
-# Learn this one.  
+# It uses the re (regular expressions) module.
+# Learn this one.
 # Rememder to use the fully qualified name.
 # -------------------------------------------------------------
     elif 'open website' in command:
@@ -186,7 +195,7 @@ def assistant(command, playcounter, songs2play, runtest):
     elif 'look' in command:
         talktome.talkToMe("Searching Wikipedia . . . ")
         command = cleanj(command)
-        #results = wikipedia.summary(command, sentences=3)
+        # results = wikipedia.summary(command, sentences=3)
         results = wikipedia.summary(command)
         wikiurl = wikipedia.page(command)
         webbrowser.open_new_tab(wikiurl.url)
@@ -199,7 +208,7 @@ def assistant(command, playcounter, songs2play, runtest):
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 # Play your music
-# Next command will choose random songs.  
+# Next command will choose random songs.
 # Set the number of songs in Juliet.py.  For example: songs2play = 2
 # -------------------------------------------------------------
     elif 'music' in command:
@@ -407,13 +416,14 @@ def assistant(command, playcounter, songs2play, runtest):
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 # Next command -- List commands uses the commandlist file
-# If you add commands to juliebrain.py, also ad the command name to commandlist.
+# If you add commands to juliebrain.py,
+# also ad the command name to commandlist.
 # -------------------------------------------------------------
     elif 'commands' in command:
         talktome.talkToMe("You can ask Julia to")
         with open("commandlist") as file:
             for line in file:
-                #line = line.strip()
+                # line = line.strip()
                 talktome.talkToMe(line)
 # -------------------------------------------------------------
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -430,5 +440,6 @@ def assistant(command, playcounter, songs2play, runtest):
 
 # End Miscelaneous Section
 
-# END GIGANTIC ASSISTANT FUNCTION -- as of 4/24/20, there are 26 commands in this brain.
+# END GIGANTIC ASSISTANT FUNCTION -- as of 4/24/20,
+# there are 26 commands in this brain.
 #############################################################
