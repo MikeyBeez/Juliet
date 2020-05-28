9f33d205523ff6d58d4ffa6c784dff74ec7e76d4
Mike Bee <mbonsign@gmail.com>
2020-05-28 14:41:24 -0400

    flake8

--- SpeakAndHear/talktome.py
+++ SpeakAndHear/talktome.py
@@ -1,5 +1,5 @@
 #############################################################
-######## TTS TEXT TO SPEECH FUNCTION 
+# TTS TEXT TO SPEECH FUNCTION
 
 # This gets used all over to speak text aloud.
 # It also prints to the console for people with bad memories.
@@ -7,19 +7,20 @@
 from gtts import gTTS
 import os
 
+
 def talkToMe(mytext):
     # "speaks audio passed as argument"
     print(mytext)
     # can handle multiline text.
-    #for line in mytext.splitlines():
-        # uses the google text to speech module to synthesize text
+    # for line in mytext.splitlines():
+    # uses the google text to speech module to synthesize text
     text_to_speech = gTTS(text=mytext, lang='en-uk')
-        # saves syntesized speech to audio.mp3
-        # this file gets written, played. and overwritten
-        # over and over again.
+    # saves syntesized speech to audio.mp3
+    # this file gets written, played. and overwritten
+    # over and over again.
     text_to_speech.save('audio.mp3')
-        # the sox modules wrapper is mpg123.
-        # This is called by the operating system imported os module.
+    # the sox modules wrapper is mpg123.
+    # This is called by the operating system imported os module.
     os.system('mpg123 -q audio.mp3')
 #############################################################
-######## END TTS TEXT TO SPEECH FUNCTION 
+# END TTS TEXT TO SPEECH FUNCTION
